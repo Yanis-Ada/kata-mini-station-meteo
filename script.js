10b3b0cea402b6bd22669cbb6febe8519fd46373
Yanis <yanis.blanchard@gmail.com>
2025-08-01 16:51:31 +0200

    stage bonus : other informations & website

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const button = document.querySelector("button");
 const gpsDiv = document.getElementById("gps");
 const detailsDiv = document.getElementById("details");
 const temperatureDiv = document.getElementById("temperature")
+const humiDiv = document.getElementById("humidity")
+const preciDiv = document.getElementById("precipitation")
 
 async function fetchCoordinates(city) {
     try {
@@ -12,17 +14,18 @@ async function fetchCoordinates(city) {
         const data = await response.json();
         const lat = data[0].lat;
         const lon = data[0].lon;
+       
         cityDiv.textContent = `${city}`
         gpsDiv.textContent = `Coordonnées GPS : ${lat}, ${lon}`; 
-        detailsDiv.textContent = "Coordonnées récupérées."
-
+        detailsDiv.textContent = "Coordonnées récupérées.";
+       
         fetchWeather(lat, lon)
 
         } catch (error) {
         gpsDiv.textContent = "";
         detailsDiv.textContent = `Erreur : ${error.message}`;
         console.error("Problème de récuperation de données", error)
-        
+
         if (input != city) {
             cityDiv.textContent = "Ville non trouvée";
             detailsDiv.textContent = "Vérifier le nom de la ville";
@@ -45,11 +48,23 @@ async function fetchCoordinates(city) {
 
 async function fetchWeather(lat, lon) {
     try {
-        const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m`;
-        const response = await fetch(weatherUrl);
-        const data = await response.json();
-        const temperature = data.current.temperature_2m;
+        const tempUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m`;
+        const tempRes = await fetch(tempUrl);
+        const tempData = await tempRes.json();
+        const temperature = tempData.current.temperature_2m;
         temperatureDiv.textContent += `${temperature} °C`;
+
+        const humiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=relative_humidity_2m`;
+        const humiRes = await fetch(humiUrl);
+        const humiData = await humiRes.json();
+        const humidity = humiData.current.relative_humidity_2m;
+        humiDiv.textContent = `Taux d'humidité : ${humidity} %`;
+        
+        const preciUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=precipitation`;
+        const preciRes = await fetch(preciUrl);
+        const preciData = await preciRes.json();
+        const precipitation = preciData.current.precipitation;
+        preciDiv.textContent = `Précipitations : ${precipitation} mm`;
 
     } catch (error) {
         detailsDiv.textContent += `Erreur météo : ${error.message}`;
